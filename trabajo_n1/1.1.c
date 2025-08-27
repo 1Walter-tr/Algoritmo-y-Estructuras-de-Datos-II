3c614fc3dd3c1d75c2ac394aef6d44b4c378b585
1Walter-tr <1waltertramirez@gmail.com>
2025-08-27 00:10:15 -0300

    Uso de punteros basicos

--- trabajo_n1/1.1.c
+++ trabajo_n1/1.1.c
@@ -1,20 +1,16 @@
-/*Declarar una variable y despues cambiar el valor de esta, para asi mostrar su valor original y su valor original*/
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-  int x = 10; //Declaramos una variable
-  int *ptr = &x; //Declaramos una variable puntero que apunta a la direccion de x
+    int edad = 42;
+    int *punteroEdad = &edad; //Accedemos a la direccion de la variable edad
 
-  printf("--- COMPRENDER VARIABLE Y DIRECCION DE MEMORIA\n");
-  printf("Valor de x: %d\n",x);
-  printf("Direccion de memoria de x: %p\n",&x); //Esto es solamente la direccion
-  printf("Valor de ptr: %p\n",ptr); //Es la misma que la direccion de x, ya que esta la apunta
-  printf("Contenido a cual apunta *ptr: %d\n",*ptr);
+    printf("Valor original: %d\n",edad);
+    printf("Valor desde puntero: %d\n", *punteroEdad); //Accedemos al valor a traves del puntero
 
-  printf("--- CAMBIAR VALOR DE VARIABLE ---\n");
-  printf("Valor original de la variable: %d\n",x);
-  *ptr = 20; //Actualizamos el valor de la variable
-  printf("Valor cambiado de la variable: %d\n",x);
+    *punteroEdad = 90; //Ahora accedemos a la direccion de memoria y a traves de ella cambiamos el valor
 
-  return 0;
+    printf("Valor nuevo desde puntero: %d\n",*punteroEdad);
+
+    return 0;
 }
